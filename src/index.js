efe26a24e6e715c1ad8c43e623a5c67dc379990b
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-01 09:24:01 -0400

    Implemented redux store in app

--- src/index.js
+++ src/index.js
@@ -5,23 +5,6 @@ import App from './components/App';
 import store from './store/index';
 import registerServiceWorker from './registerServiceWorker';
 
-const stories = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    }, {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
 
 ReactDOM.render(<App stories={ store.getState() } onArchieve={ () => {} }/>, document.getElementById('root'));
 registerServiceWorker();
