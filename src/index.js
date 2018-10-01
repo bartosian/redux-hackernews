d996ec1644bf1865419006c7c6825f315d57c468
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-01 10:21:37 -0400

    Added dispatch method to onArchive feature

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import store from './store/index';
+import { STORY_ARCHIVE } from "./constants/actionTypes";
 import registerServiceWorker from './registerServiceWorker';
-console.log(store.getState());
 
-ReactDOM.render(<App stories={ store.getState().storyState } onArchieve={ () => {} }/>, document.getElementById('root'));
+ReactDOM.render(<App stories={ store.getState().storyState } onArchieve={ id =>  store.dispatch({ type: STORY_ARCHIVE, id })}/>, document.getElementById('root'));
 registerServiceWorker();
